eb8c00e70ba35868e02357053d16e58a04a96a3c
丶Lee.R <782707826@qq.com>
2019-10-15 17:48:51 +0800

    mid.js

---
+++ mid/mid.js
@@ -0,0 +1,157 @@
+class opt {
+	constructor(host) {
+	    this.host=host;
+	}
+	wrap_host(uri){
+		return this.host+uri;
+	}
+	
+	read(key) {
+		return new Promise((resolve, reject) => {
+			uni.getStorage({
+				key: key,
+				success: function(res) {},
+				complete: function(res) {
+					resolve(res.data)
+				}
+			})
+		})
+	}
+	write(key, value) {
+		return new Promise((resolve, reject) => {
+			uni.setStorage({
+				key: key,
+				data: value,
+				complete: function(r) {
+					resolve(r);
+				}
+			})
+		})
+	}
+	to(uri){
+		uni.reLaunch({
+			url: uri
+		});
+	}
+	navigateTo(uri){
+		uni.navigateTo({
+			url:uri
+		});
+	}
+	auth_to(uri) {
+		default_req.get_().then(d => {
+			uni.reLaunch({
+				url: uri
+			});
+		}).catch(e => {
+			uni.reLaunch({
+				url: ""
+			});
+		})
+	}
+	base(method, url, data, opt = {}) {
+		return new Promise((resolve, reject) => {
+			let header = {};
+			if (opt.header) {
+				header = opt.header;
+			}
+			uni.request({
+				url: this.wrap_host(url),
+				header: header,
+				method: method,
+				data: data,
+				success: res => {
+					if (res.statusCode != 200) {
+						console.log("--------------->失败",res)
+						if (res.data) {
+							if(res.data.message==='token'){
+								uni.showToast({
+									icon:'none',
+									title:'登录过期，请重新登录。'
+								},1000);
+								setTimeout(()=>{
+									uni.removeStorage({
+										key: 'token',									
+									})
+									uni.reLaunch({
+										url:'../sign/sign'
+									})
+								},300)
+							}else{
+								uni.showToast({
+									icon:'none',
+									title: res.data.message,
+								},1000);
+							}
+						}
+					} else {
+						if(res.data.message){
+							uni.showToast({
+								title: res.data.message,
+							},1000);
+							setTimeout(()=>{resolve(res.data)},300)
+						}else{
+							resolve(res.data);
+						}					
+						
+					}
+				},
+				fail: () => {
+					reject("fail")
+				},
+				complete: () => {
+					// reject("fail")
+				}
+			});
+		}).catch(t=>{
+			console.log("----------------->t",t)
+			if(t.data!=null && t.data.message!=null ){
+				uni.showToast({
+					title: t.data.message,
+					duration: 5500		
+				});
+				uni.reLaunch({
+					url: '/pages/tabBar/commonTabBar.vue'
+				})
+			}else{
+				uni.showToast({
+					title: t,				
+				},88500);
+			}
+			
+		});
+		
+	}
+	//普通的get
+	get(url, data) {
+		return this.base('GET', url, data);
+	}
+	post(url, data) {
+		return this.base("POST", url, data)
+	}
+	//自带token的get
+	get_(url, data) {
+		return this.read("token").then(d => {
+			return this.base('GET', url, data, {
+				header: {"token":d.data}
+			})
+		})
+	}
+	post_(url, data) {
+		return this.read("token").then(d => {
+			console.log(d,"----------->token")
+			return this.base('POST', url, data, {
+				header:  {"token":d.data}
+			})
+		})
+	}	
+	
+}
+
+
+
+/* http://192.168.0.115:8761 */
+/* https://sys.xwkeji.net */
+
+export default new opt('https://sys.xwkeji.net');
+
